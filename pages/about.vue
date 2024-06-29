d634a27b2c0f36b43d9b6833408e0a7ce27eef53
weiguic2w <wcw1913@163.com>
2024-06-29 23:27:50 +0800

    feat: build about page

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="con">
+    <main>
+      <h2>数据来源</h2>
+      <p>
+        文章来源于<a href="https://www.foreignaffairs.com/" target="_blank"
+          >foreignaffairs</a
+        >的一个镜像网站；翻译由<a
+          href="https://tongyi.aliyun.com/qianwen/"
+          target="_blank"
+          >qwen-plus</a
+        >大模型以及<a href="https://ai.youdao.com/" target="_blank">有道智云</a
+        >提供。
+      </p>
+      <h2>为什么使用ai翻译？为什么使用qwen-plus？</h2>
+      <p>
+        ai翻译有效减少了传统机翻的僵硬，译文更为准确；经个人测试qwen-plus在中文翻译支持方面同比chat-gpt4更为出色。同时由于文章涉及敏感内容，该部分翻译由有道智云提供支持。
+      </p>
+      <h2>如何查词？</h2>
+      <p>
+        网站不提供查词功能，如需要请下载<a
+          href="https://chromewebstore.google.com/detail/%E6%AC%A7%E8%B7%AF%E7%BF%BB%E8%AF%91-%E7%BD%91%E9%A1%B5%E5%88%92%E8%AF%8D%E7%BF%BB%E8%AF%91%E5%B7%A5%E5%85%B7/djbfechcnkppbknmlhfcaoifgnicolin"
+          target="_blank"
+          >欧路翻译</a
+        >插件。
+      </p>
+      <h2>文章涉及政治色彩？</h2>
+      <p>
+        <strong style="color: var(--primary-100)"
+          >文章带有主观的政治立场并不代表双维本人观点，请自行辨别！</strong
+        >目前精力有限暂时爬取静态网站来充当数据源，
+      </p>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.con {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  font-size: 2rem;
+}
+a {
+  text-decoration: underline;
+}
+h2 {
+  padding-top: 2rem;
+}
+</style>
